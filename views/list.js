a184958bf51265cb4e73507aaeb1e8e6c524b9b7
Guillaume Claverie <gclaverie0@gmail.com>
2024-12-19 11:58:45 +0100

    feat: add search by title & author

--- views/list.js
+++ views/list.js
@@ -1,9 +1,8 @@
 import createBookTemplate from "./book.js";
-import {BOOKS_DATA} from "../data/data.js";
 
-const createListTemplate = () => `
+const createListTemplate = (books) => `
     <ul>
-        ${BOOKS_DATA.map((book) => createBookTemplate(book)).join('')}
+        ${books.map((book) => createBookTemplate(book)).join('')}
     </ul>
 `
 export default createListTemplate;
